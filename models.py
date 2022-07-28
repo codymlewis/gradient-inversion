312987cc6eae8b073f45364ef8c7871b3ef9098d
Cody Lewis <cody@codymlewis.com>
2022-07-28 19:22:43 +1000

    Removed redundant code

--- models.py
+++ models.py
@@ -63,14 +63,3 @@ class CNN(nn.Module):
             return x
         x = nn.Dense(10, name="classifier")(x)
         return nn.softmax(x)
-        return nn.Sequential(
-            [
-                nn.Conv(32, (3, 3)), nn.relu,
-                lambda x: nn.max_pool(x, (2, 2)),
-                nn.Conv(64, (3, 3)), nn.relu,
-                lambda x: nn.max_pool(x, (2, 2)),
-                lambda x: einops.rearrange(x, "b w h c -> b (w h c)"),
-                nn.Dense(100), nn.relu,
-                nn.Dense(10, name="classifier"), nn.softmax,
-            ]
-        )(x)
